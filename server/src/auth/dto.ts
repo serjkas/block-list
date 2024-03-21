2330ab55f85d0adcdf308252488bb1e4fe63d53e
Sergey Ka <63855768+serjkas@users.noreply.github.com>
2024-03-21 20:29:01 +0300

    validation

--- server/src/auth/dto.ts
+++ server/src/auth/dto.ts
@@ -1,21 +1,26 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsNotEmpty } from 'class-validator';
 
 export class SingUpDto {
   @ApiProperty({
     example: 'a@a.com',
   })
+  @IsEmail()
   email: string;
 
   @ApiProperty({
     example: '1234',
   })
+  @IsNotEmpty()
   password: string;
 }
 
 export class SingInDto {
   @ApiProperty({ example: 'a@a.com' })
+  @IsEmail()
   email: string;
   @ApiProperty({ example: 'aaa' })
+  @IsNotEmpty()
   password: string;
 }
 
